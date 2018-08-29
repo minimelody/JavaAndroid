3032a9584c49b84485071058050fb67f697951a1
minimelody <minimelodi@naver.com>
2018-08-29 20:16:54 +0900

    print, println

--- Day_01/src/Exam_01.java
+++ Day_01/src/Exam_01.java
@@ -1,7 +1,20 @@
 import java.lang.*;
 
+// GNU Style, BSD Style, K&R Style
+
 public class Exam_01 {
 	public static void main(String [] args) {
-		System.out.println("Hello world");
+		System.out.print("Hello World");
+		System.out.print("나는 오늘 자바를 처음 시작합니다.");
+		System.out.print(10-5);
+		System.out.print("10"); 
+		System.out.print('A');
+		
+		System.out.println("TEST");
+		System.out.println("ABC");
+
+		// 문자열 출력은 ""(쌍따옴표)
+		// 숫자 출력은 No 따옴표
+		// 한글자 출력은 ''(홑따옴표)
 	}
 }
